7596dd1c65b4b9431cf144d3b5b16bbe781fd6b7
FES001 <firdavs7794948@gmail.com>
2024-11-13 01:49:43 +0500

    working on it.

--- scripts/start-game.js
+++ scripts/start-game.js
@@ -1,11 +1,12 @@
 const startTheGameButton = document.getElementById('startTheGameButton');
 const startMenuControls = document.querySelector('.game-controls');
 const gameItselfContainer = document.querySelector('.game-itself');
+const fruitsImages = ["../assets/fruits/fruit1.png","../assets/fruits/fruit2.png","../assets/fruits/fruit3.png","../assets/fruits/fruit4.png", "../assets/fruits/fruit5.png", "../assets/fruits/fruit6.png", "../assets/fruits/fruit7.png"];
 let gameIsStarted = false;
 
 // START THE GAME FUNCTION
 
-function startGame() {
+/* function startGame() {
     gameIsStarted = true;
 
     startTheGameButton.textContent = 'Restart';
@@ -15,4 +16,17 @@ function startGame() {
 
 // START THE GAME BUTTON
 
-startTheGameButton.addEventListener('click', startGame);
+startTheGameButton.addEventListener('click', startGame); */
+
+
+function randomFruits() {
+    for (let i = 0; i < 7; i++) {
+        const fruitItself = document.createElement('div');
+        fruitItself.classList.add('game-itself-image');
+        fruitItself.innerHTML = `
+            <img class="game-itself-image-itself" src="${fruitsImages[i]}">
+        `;
+
+        gameItselfContainer.appendChild(fruitItself);
+    };
+};
