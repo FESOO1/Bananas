4ec5d87d48c2e95ba2f38516e483f2c6e5496bcc
FES001 <firdavs7794948@gmail.com>
2024-11-13 20:58:10 +0500

    working on it.

--- scripts/start-game.js
+++ scripts/start-game.js
@@ -2,22 +2,26 @@ const startTheGameButton = document.getElementById('startTheGameButton');
 const startMenuControls = document.querySelector('.game-controls');
 const gameItselfContainer = document.querySelector('.game-itself');
 const gameItselfScoreText = document.querySelector('.game-itself-score');
-const fruitsImages = ["../assets/fruits/fruit1.png","../assets/fruits/fruit2.png","../assets/fruits/fruit3.png","../assets/fruits/fruit4.png", "../assets/fruits/fruit5.png", "../assets/fruits/fruit6.png", "../assets/fruits/fruit7.png","../assets/fruits/fruit1.png","../assets/fruits/fruit2.png","../assets/fruits/fruit3.png","../assets/fruits/fruit4.png", "../assets/fruits/fruit5.png", "../assets/fruits/fruit6.png", "../assets/fruits/fruit7.png","../assets/fruits/fruit1.png","../assets/fruits/fruit2.png","../assets/fruits/fruit3.png","../assets/fruits/fruit4.png", "../assets/fruits/fruit5.png", "../assets/fruits/fruit6.png"];
+const gameItselfMissedText = document.querySelector('.game-itself-missed');
+const fruitsImages = ["../assets/fruits/fruit1.png","../assets/fruits/fruit2.png","../assets/fruits/fruit3.png","../assets/fruits/fruit4.png", "../assets/fruits/fruit5.png", "../assets/fruits/fruit6.png", "../assets/fruits/fruit7.png"];
 let gameIsStarted = false;
 let scorePoint = 0;
+let missedPoint = 0;
 
 // START THE GAME FUNCTION
 
 function startGame() {
     gameIsStarted = true;
-
+    
     startTheGameButton.textContent = 'Restart';
     startMenuControls.classList.add('game-controls-hidden');
     gameItselfContainer.classList.add('game-itself-active');
 };
 
 function randomFruits() {
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 7; i++) {
+        const top = '100%';
+        const randomTop = Math.floor(Math.random() * 250);
         const fruitItself = document.createElement('div');
         fruitItself.classList.add('game-itself-image');
         fruitItself.innerHTML = `
@@ -31,8 +35,8 @@ function randomFruits() {
 
         // RANDOM DROPPING
         fruitItself.style.left = Math.floor(Math.random() * 100) + '%';
-        fruitItself.style.top = '-' +  Math.floor(Math.random() * 250) + 'px';
-        /* fruitItself.style.animation = Math.floor(Math.random() * 25) + 's infinite fruit-image-dropping'; */
+        fruitItself.style.top = '-' +  randomTop + 'px';
+        /* fruitItself.style.animation = Math.floor(Math.random() * 25) + 's fruit-image-dropping'; */
 
         // CLICK ON A FRUIT
         fruitItself.addEventListener('click', () => {
@@ -40,11 +44,11 @@ function randomFruits() {
             gameItselfScoreText.textContent = `Score: ${scorePoint}`;
             fruitItself.classList.remove('game-itself-image-dropping');
             /* fruitItself.style.animation = 'none'; */
-            fruitItself.style.top = '-' +  Math.floor(Math.random() * 250) + 'px';
+            fruitItself.style.top = '-' +  randomTop + 'px';
             fruitItself.style.left = Math.floor(Math.random() * 100) + '%';
             setTimeout(() => {
                 fruitItself.classList.add('game-itself-image-dropping');
-                /* fruitItself.style.animation = Math.floor(Math.random() * 25) + 's infinite fruit-image-dropping'; */
+                /* fruitItself.style.animation = Math.floor(Math.random() * 25) + 's fruit-image-dropping'; */
             }, 100);
         });
     };
